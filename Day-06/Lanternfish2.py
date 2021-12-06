90655c3822bfb4130c328491df99118b80ef9bcb
Paul Suh <paul@mspex.net>
2021-12-06 01:14:26 -0500

    Day 06

---
+++ Day-06/Lanternfish2.py
@@ -0,0 +1,35 @@
+import sys
+import fileinput
+import functools
+
+
+@functools.cache
+def calc_growth( fish_timer: int, days_to_end: int ) -> int:
+    # not enough time to reproduce
+    if days_to_end < fish_timer+1:
+        return 1
+
+    return calc_growth( 6, days_to_end-(fish_timer+1) ) + calc_growth( 8, days_to_end-(fish_timer+1) )
+
+
+def main() -> int:
+    for line in fileinput.input():
+        fish_list = [
+            int(x) for x in line.rstrip().split(",")
+        ]
+
+    days_result = [
+        calc_growth(x, 256) for x in range(7)
+    ]
+
+    print( sum(
+        [
+            days_result[x] for x in fish_list
+        ]
+    ))
+
+    return 0
+
+
+if __name__ == '__main__':
+    sys.exit(main())
